c9fda2da5c28571ebeb1b80d012a2134b54f1ad0
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-01-25 17:12:11 -0600

    Add function to change weather UI elements values

--- src/modules/API.js
+++ src/modules/API.js
@@ -13,18 +13,48 @@ const getWeather = async (location) => {
   const allWeather = await getAllWeather(location);
   const weather = {};
 
-  weather.temp_c = allWeather.current.temp_c;
-  weather.temp_f = allWeather.current.temp_f;
-  weather.feelsLike_c = allWeather.current.feelsLike_c;
-  weather.feelsLike_f = allWeather.current.feelsLike_f;
+  weather.tempC = allWeather.current.temp_c;
+  weather.tempF = allWeather.current.temp_f;
+  weather.feelsLikeC = allWeather.current.feelslike_c;
+  weather.feelsLikeF = allWeather.current.feelslike_f;
   weather.humidity = allWeather.current.humidity;
-  weather.wind_kph = allWeather.current.wind_kph;
-  weather.wind_mph = allWeather.current.wind_mph;
+  weather.windKPH = allWeather.current.wind_kph;
+  weather.windMPH = allWeather.current.wind_mph;
   weather.condition = allWeather.current.condition.text;
-  weather.icon = allWeather.current.condition.icon.substring(2);
-  weather.name = allWeather.location.name;
+  weather.icon = allWeather.current.condition.icon;
+  weather.city = allWeather.location.city;
+  weather.region = allWeather.location.region;
+  [, weather.time] = allWeather.current.last_updated.split(' ');
+
+  console.log(weather);
 
   return weather;
 };
 
-export default getWeather;
+const changeWeather = (location) => {
+  getWeather(location).then((weather) => {
+    const icon = document.querySelector('.current .icon');
+    const temperatureC = document.querySelector('.current .temperatureC');
+    const temperatureF = document.querySelector('.current .temperatureF');
+
+    const condition = document.querySelector('.current .condition');
+    const feelsLikeC = document.querySelector('.current .feelsLikeC');
+    const feelsLikeF = document.querySelector('.current .feelsLikeF');
+    const humidity = document.querySelector('.current .humidity');
+    const windKPH = document.querySelector('.current .windKPH');
+    const windMPH = document.querySelector('.current .windMPH');
+
+    icon.src = weather.icon;
+    temperatureC.textContent = `${weather.tempC}°C`;
+    temperatureF.textContent = `${weather.tempF}°F`;
+
+    condition.textContent = weather.condition;
+    feelsLikeC.textContent = `Feels Like: ${weather.feelsLikeC}°C`;
+    feelsLikeF.textContent = `Feels Like: ${weather.feelsLikeF}°F`;
+    humidity.textContent = `Humidity: ${weather.humidity}%`;
+    windKPH.textContent = `Wind: ${weather.windKPH}KPH`;
+    windMPH.textContent = `Wind: ${weather.windMPH}MPH`;
+  });
+};
+
+export default changeWeather;
