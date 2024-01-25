1b4df8203f4bf77e2bf4f95cc6441553d5c83a6c
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-01-25 16:20:58 -0600

    Add function to get only necessary weather data from request

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,5 +1,4 @@
-const getWeather = async () => {
-  const location = prompt('Give location');
+const getAllWeather = async (location) => {
   const key = '219ec63ed2a0417982e43106241901';
 
   const unformattedWeather = await fetch(
@@ -7,7 +6,25 @@ const getWeather = async () => {
   );
   const weather = await unformattedWeather.json();
 
-  console.log(weather);
+  return weather;
+};
+
+const getWeather = async (location) => {
+  const allWeather = await getAllWeather(location);
+  const weather = {};
+
+  weather.temp_c = allWeather.current.temp_c;
+  weather.temp_f = allWeather.current.temp_f;
+  weather.feelsLike_c = allWeather.current.feelsLike_c;
+  weather.feelsLike_f = allWeather.current.feelsLike_f;
+  weather.humidity = allWeather.current.humidity;
+  weather.wind_kph = allWeather.current.wind_kph;
+  weather.wind_mph = allWeather.current.wind_mph;
+  weather.condition = allWeather.current.condition.text;
+  weather.icon = allWeather.current.condition.icon.substring(2);
+  weather.name = allWeather.location.name;
+
+  return weather;
 };
 
 export default getWeather;
