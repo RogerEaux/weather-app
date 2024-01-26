86fbea42c5cfdd03a8b6a72f1ad2e42973cecdba
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-01-26 15:33:08 -0600

    Add function to create forecast containers

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -16,95 +16,10 @@ const createHeader = () => {
   return header;
 };
 
-const createMaxMinContainer = () => {
-  const maxMinContainer = document.createElement('div');
-  const maxC = document.createElement('p');
-  const maxF = document.createElement('p');
-  const minC = document.createElement('p');
-  const minF = document.createElement('p');
-
-  maxMinContainer.classList = 'maxMin';
-  maxC.classList = 'maxC';
-  maxF.classList = 'maxF';
-  minC.classList = 'minC';
-  minF.classList = 'minF';
-
-  maxMinContainer.append(maxC, maxF, minC, minF);
-
-  return maxMinContainer;
-};
-
-const createTempContainer = () => {
-  const tempContainer = document.createElement('div');
-  const icon = document.createElement('img');
-  const temperatureC = document.createElement('p');
-  const temperatureF = document.createElement('p');
-  const maxMinContainer = createMaxMinContainer();
-
-  tempContainer.classList = 'temp';
-  icon.classList = 'icon';
-  temperatureC.classList = 'temperatureC';
-  temperatureF.classList = 'temperatureF';
-
-  tempContainer.append(icon, temperatureC, temperatureF, maxMinContainer);
-
-  return tempContainer;
-};
-
-const createWeatherMini = () => {
-  const miniContainer = document.createElement('div');
-  const tempContainer = createTempContainer();
-  const location = document.createElement('p');
-  const time = document.createElement('p');
-
-  miniContainer.classList = 'mini';
-
-  location.classList = 'location';
-  time.classList = 'time';
-
-  miniContainer.append(tempContainer, location, time);
-
-  return miniContainer;
-};
-
-const createCurrentWeather = () => {
-  const currentWeather = document.createElement('div');
-  const detailsContainer = document.createElement('div');
-  const condition = document.createElement('p');
-  const feelsLikeC = document.createElement('p');
-  const feelsLikeF = document.createElement('p');
-  const humidity = document.createElement('p');
-  const windKPH = document.createElement('p');
-  const windMPH = document.createElement('p');
-
-  currentWeather.classList = 'current';
-  detailsContainer.classList = 'details';
-  condition.classList = 'condition';
-  feelsLikeC.classList = 'feelsLikeC';
-  feelsLikeF.classList = 'feelsLikeF';
-  humidity.classList = 'humidity';
-  windKPH.classList = 'windKPH';
-  windMPH.classList = 'windMPH';
-
-  detailsContainer.append(
-    condition,
-    feelsLikeC,
-    feelsLikeF,
-    humidity,
-    windKPH,
-    windMPH,
-  );
-  currentWeather.append(createWeatherMini(), detailsContainer);
-
-  return currentWeather;
-};
-
-const createMain = () => {
-  const main = document.createElement('main');
+const createSearch = () => {
   const search = document.createElement('div');
   const searchInput = document.createElement('input');
   const searchButton = document.createElement('button');
-  const currentWeather = createCurrentWeather();
 
   search.classList = 'search';
   searchInput.setAttribute('type', 'text');
@@ -118,7 +33,130 @@ const createMain = () => {
   });
 
   search.append(searchInput, searchButton);
-  main.append(search, currentWeather);
+
+  return search;
+};
+
+const createTempContainer = () => {
+  const tempContainer = document.createElement('div');
+  const icon = document.createElement('img');
+  const temperatureC = document.createElement('p');
+  const temperatureF = document.createElement('p');
+
+  tempContainer.classList = 'temp';
+  icon.classList = 'icon';
+  temperatureC.classList = 'temperatureC';
+  temperatureF.classList = 'temperatureF';
+
+  tempContainer.append(icon, temperatureC, temperatureF);
+
+  return tempContainer;
+};
+
+const createWeatherMini = () => {
+  const miniContainer = document.createElement('div');
+  const condition = document.createElement('p');
+  const location = document.createElement('p');
+  const time = document.createElement('p');
+
+  miniContainer.classList = 'mini';
+  condition.classList = 'condition';
+  location.classList = 'location';
+  time.classList = 'time';
+
+  miniContainer.append(createTempContainer(), condition, location, time);
+
+  return miniContainer;
+};
+
+const createDetailsContainer = () => {
+  const detailsContainer = document.createElement('div');
+
+  const feelsLikeC = document.createElement('p');
+  const feelsLikeF = document.createElement('p');
+  const precipitation = document.createElement('p');
+  const humidity = document.createElement('p');
+  const windKPH = document.createElement('p');
+  const windMPH = document.createElement('p');
+
+  detailsContainer.classList = 'details';
+
+  feelsLikeC.classList = 'feelsLikeC';
+  feelsLikeF.classList = 'feelsLikeF';
+  precipitation.classList = 'precipitation';
+  humidity.classList = 'humidity';
+  windKPH.classList = 'windKPH';
+  windMPH.classList = 'windMPH';
+
+  detailsContainer.append(
+    feelsLikeC,
+    feelsLikeF,
+    precipitation,
+    humidity,
+    windKPH,
+    windMPH,
+  );
+
+  return detailsContainer;
+};
+
+const createCurrentWeather = () => {
+  const currentWeather = document.createElement('div');
+
+  currentWeather.classList = 'current';
+  currentWeather.append(createWeatherMini(), createDetailsContainer());
+
+  return currentWeather;
+};
+
+const createMaxMinContainer = () => {
+  const maxMinContainer = document.createElement('div');
+  const icon = document.createElement('img');
+  const maxC = document.createElement('p');
+  const maxF = document.createElement('p');
+  const minC = document.createElement('p');
+  const minF = document.createElement('p');
+
+  maxMinContainer.classList = 'maxMin';
+  icon.classList = 'icon';
+  maxC.classList = 'maxC';
+  maxF.classList = 'maxF';
+  minC.classList = 'minC';
+  minF.classList = 'minF';
+
+  maxMinContainer.append(icon, maxC, maxF, minC, minF);
+
+  return maxMinContainer;
+};
+
+const createForecastContainer = () => {
+  const forecastContainer = document.createElement('div');
+
+  forecastContainer.classList = 'day';
+
+  forecastContainer.append(createMaxMinContainer());
+
+  return forecastContainer;
+};
+
+const createForecastWeather = () => {
+  const forecasteWeather = document.createElement('div');
+
+  forecasteWeather.classList = 'forecast';
+
+  forecasteWeather.append(
+    createForecastContainer(),
+    createForecastContainer(),
+    createForecastContainer(),
+  );
+
+  return forecasteWeather;
+};
+
+const createMain = () => {
+  const main = document.createElement('main');
+
+  main.append(createSearch(), createCurrentWeather(), createForecastWeather());
 
   return main;
 };
